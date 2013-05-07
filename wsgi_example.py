3486697e47ba3e9112a11bc185ee14d54e038162
hypertexthero <simon@hypertexthero.com>
2013-05-07 09:31:52 +0200

    settings, renaming

--- wsgi_example.py
+++ wsgi_example.py
@@ -3,14 +3,14 @@
 import os, sys, site
  
 # add virtualenv python libs
-site.addsitedir('/home/<yourusername>/webapps/<yourappname>/<yourvirtualenvname>/lib/python2.7/site-packages')
+site.addsitedir('/opt/projects/itwishlist-env/lib/python2.6/site-packages')
  
 # append the project path to system path
-sys.path.append('/home/<yourusername>/webapps/<yourappname>/<yourvirtualenvname>/')
-sys.path.append('/home/<yourusername>/webapps/<yourappname>/<yourvirtualenvname>/<yourprojectname>')
+sys.path.append('/opt/projects/itwishlist-env/')
+sys.path.append('/opt/projects/itwishlist-env/itwishlist')
  
 # set the settings module
-os.environ['DJANGO_SETTINGS_MODULE'] = '<yourprojectname>.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'itwishlist.settings'
  
 # init the wsgi handler
 # from django.core.handlers.wsgi import WSGIHandler
